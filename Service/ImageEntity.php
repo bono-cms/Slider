7e80a14380e0aba3ec0886468819a43a8a2c31a1
David Yang <daworld.ny@gmail.com>
2016-04-07 21:17:37 +0500

    Added getImageUrl() in entity

--- Service/ImageEntity.php
+++ Service/ImageEntity.php
@@ -16,6 +16,17 @@ use Krystal\Stdlib\VirtualEntity;
 final class ImageEntity extends VirtualEntity
 {
     /**
+     * Returns image URL
+     * 
+     * @param string $size
+     * @return string
+     */
+    public function getImageUrl($size)
+    {
+        return $this->getImageBag()->getUrl($size);
+    }
+
+    /**
      * Tells whether a slide image has a link
      * 
      * @return boolean
