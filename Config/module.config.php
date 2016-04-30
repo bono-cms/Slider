9933f4b0fc56a6bed2f09c7f7ea4b90039bf9f0e
David Yang <daworld.ny@gmail.com>
2016-04-30 22:38:06 +0500

    Replaced raw path with route

--- Config/module.config.php
+++ Config/module.config.php
@@ -12,7 +12,7 @@
 return array(
     'module' => array(
         'name'  => 'Slider',
-        'url'   => '/admin/module/slider',
+        'route' => 'Slider:Admin:Browser@indexAction',
         'icon'  => 'fa fa-picture-o fa-5x',
         'order' => 1,
         'description' => 'Slider module allows you to manage sliders with random dimensions'
