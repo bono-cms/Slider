df3a79e0ee6bb65c64659fb902aca1fa0a782b75
David Yang <daworld.ny@gmail.com>
2016-08-01 22:29:38 +0500

    Removed extras from module configuration file

--- Config/module.config.php
+++ Config/module.config.php
@@ -1,20 +1,9 @@
 <?php
 
-/**
- * This file is part of the Bono CMS
- * 
- * Copyright (c) No Global State Lab
- * 
- * For the full copyright and license information, please view
- * the license file that was distributed with this source code.
- */
-
 return array(
-    'module' => array(
-        'name'  => 'Slider',
-        'route' => 'Slider:Admin:Browser@indexAction',
-        'icon'  => 'fa fa-picture-o fa-5x',
-        'order' => 1,
-        'description' => 'Slider module allows you to manage sliders with random dimensions'
-    )
+ 'name'  => 'Slider',
+ 'route' => 'Slider:Admin:Browser@indexAction',
+ 'icon'  => 'fa fa-picture-o fa-5x',
+ 'order' => 1,
+ 'description' => 'Slider module allows you to manage sliders with random dimensions'
 );
