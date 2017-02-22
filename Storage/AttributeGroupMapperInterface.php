ecabcd27052480e7b4967c5f7b1a27840a9c9402
David Yang <daworld.ny@gmail.com>
2017-02-22 15:47:38 +0500

    Group mapper implementation

--- Storage/AttributeGroupMapperInterface.php
+++ Storage/AttributeGroupMapperInterface.php
@@ -13,5 +13,27 @@ namespace Slider\Storage;
 
 interface AttributeGroupMapperInterface
 {
-    
+    /**
+     * Deletes a group by its ID
+     * 
+     * @param string $id
+     * @return boolean
+     */
+    public function deleteById($id);
+
+    /**
+     * Fetches group data by its ID
+     * 
+     * @param string $id
+     * @return array
+     */
+    public function fetchById($id);
+
+    /**
+     * Fetches all groups associated with category ID
+     * 
+     * @param string $id Category ID
+     * @return array
+     */
+    public function fetchAll($id);
 }
--- Storage/MySQL/AttributeGroupMapper.php
+++ Storage/MySQL/AttributeGroupMapper.php
@@ -23,4 +23,42 @@ final class AttributeGroupMapper extends AbstractMapper implements AttributeGrou
     {
         return self::getWithPrefix('bono_module_slider_category_attribute_groups');
     }
+
+    /**
+     * Deletes a group by its ID
+     * 
+     * @param string $id
+     * @return boolean
+     */
+    public function deleteById($id)
+    {
+        return $this->deleteByPk($id);
+    }
+
+    /**
+     * Fetches group data by its ID
+     * 
+     * @param string $id
+     * @return array
+     */
+    public function fetchById($id)
+    {
+        return $this->findByPk($id);
+    }
+
+    /**
+     * Fetches all groups associated with category ID
+     * 
+     * @param string $id Category ID
+     * @return array
+     */
+    public function fetchAll($id)
+    {
+        return $this->db->select('*')
+                        ->from(self::getTableName())
+                        ->whereEquals('category_id', $id)
+                        ->orderBy('id')
+                        ->desc()
+                        ->queryAll();
+    }
 }
