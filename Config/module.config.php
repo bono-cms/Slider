de9ae42724b2eb9f2bcb00c0640042827b08b959
dave-yang <daworld.ny@gmail.com>
2019-03-05 23:30:59 +0500

    Update module.config.php

--- Config/module.config.php
+++ Config/module.config.php
@@ -1,10 +1,11 @@
 <?php
 
+/**
+ * Module configuration container
+ */
+
 return array(
     'name'  => 'Slider',
-    'caption'  => 'Slider',
-    'route' => 'Slider:Admin:Browser@indexAction',
-    'order' => 1,
     'description' => 'Slider module allows you to manage sliders with random dimensions',
     // Bookmarks of this module
     'bookmarks' => array(
@@ -12,6 +13,20 @@ return array(
             'name' => 'Add a slider',
             'controller' => 'Slider:Admin:Image@addAction',
             'icon' => 'glyphicon glyphicon-picture'
+        )
+    ),
+    'menu' => array(
+        'name' => 'Slider',
+        'icon' => 'fas fa-poll',
+        'items' => array(
+            array(
+                'route' => 'Slider:Admin:Browser@indexAction',
+                'name' => 'View all slides'
+            ),
+            array(
+                'route' => 'Slider:Admin:Category@addAction',
+                'name' => 'Add a slider'
+            )
         )
     )
 );
